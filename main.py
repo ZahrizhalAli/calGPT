e807bbded8ea10d80004710ca8d9c467a03396be
ZahrizhalAli <zzahrizhal.ali@gmail.com>
2023-08-22 10:01:54 +0700

    Simple Bigram model

--- main.py
+++ main.py
@@ -57,39 +57,50 @@ def qa_bot():
 
     return qa
 
+
 #output function
 def final_result(query):
     qa_result = qa_bot()
     response = qa_result({'query': query})
     return response
 
+if __name__ == "__main__":
+    while True:
+        user_query = input("Enter your query (or 'exit' to quit): ")
+        if user_query.lower() == "exit":
+            break
+        else:
+            result = final_result(user_query)
+            print("Answer:", result['answer'])
+            print("Context:", result['context'])
+            print("-----------------------------")
 #chainlit code
-@cl.on_chat_start
-async def start():
-    chain = qa_bot()
-    msg = cl.Message(content="Starting the bot...")
-    await msg.send()
-    msg.content = "Hi, Welcome to Medical Bot DEMO. What is your query?"
-    await msg.update()
-
-    cl.user_session.set("chain", chain)
-
-@cl.on_message
-async def main(message):
-    chain = cl.user_session.get("chain")
-    cb = cl.AsyncLangchainCallbackHandler(
-        stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
-    )
-    cb.answer_reached = True
-    res = await chain.acall(message, callbacks=[cb])
-    answer = res["result"]
-    sources = res["source_documents"]
-
-    if sources:
-        answer += f"\nSources:" + str(sources)
-    else:
-        answer += "\nNo sources found"
-
-    await cl.Message(content=answer).send()
+# @cl.on_chat_start
+# async def start():
+#     chain = qa_bot()
+#     msg = cl.Message(content="Starting the bot...")
+#     await msg.send()
+#     msg.content = "Hi, Welcome to Medical Bot DEMO. What is your query?"
+#     await msg.update()
+#
+#     cl.user_session.set("chain", chain)
+#
+# @cl.on_message
+# async def main(message):
+#     chain = cl.user_session.get("chain")
+#     cb = cl.AsyncLangchainCallbackHandler(
+#         stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
+#     )
+#     cb.answer_reached = True
+#     res = await chain.acall(message, callbacks=[cb])
+#     answer = res["result"]
+#     sources = res["source_documents"]
+#
+#     if sources:
+#         answer += f"\nSources:" + str(sources)
+#     else:
+#         answer += "\nNo sources found"
+#
+#     await cl.Message(content=answer).send()
 
 # Run chainlit: chainlit run main.py -w
